bcf3ecc09673ca0fd83a0e1e1abf33704a873368
Biswajit <patrabiswajit133@gmail.com>
2019-12-22 17:01:11 +0530

    added ejs as template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
+
+
+router.get('/login',function(req,res,next){
+   res.render('index');
+});
+
 
 module.exports = router;
