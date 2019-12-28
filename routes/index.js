35bc69476ae40b8ede7771320d1a91e30b3a34bd
Tulsi Prasad <tulsi.prasad50@gmail.com>
2019-12-28 20:16:34 +0530

    :sparkles: build: connect mongo for login-register
    add connection to mongodb through mongoose to run the registration and login successfully

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
     res.render('index');
 });
 
 
-router.get('/register', function(req,res,next){
-   res.render('register');
+router.get("/register", function (req, res, next) {
+    res.render("register");
+});
+
+router.get("/login", function (req, res, next) {
+    res.render("login");
 });
 
 
