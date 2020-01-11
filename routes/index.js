3783601a73f5ba92c6ae78e587a58473e7cbdfc3
Satyajeet Malla <satyajeet.malla2@gmail.com>
2020-01-12 02:31:20 +0530

    fonts are changed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.render('index');
+    res.render('index', );
 });
 
 router.get("/register", function (req, res, next) {
@@ -29,7 +29,5 @@ router.get('/manevents', function(req,res,next){
 router.get('/comingsoon', function(req,res,next){
 	res.render('comingsoon');
 });
-
-
 
 module.exports = router;
