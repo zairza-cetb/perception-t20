951a29848608d0ed64dfde9834441786026d0fc2
Satyajeet Malla <satyajeet.malla2@gmail.com>
2020-01-18 01:28:58 +0530

    code modularity is done

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,74 @@
 var express = require('express');
+var User = require("./model");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-    res.render('index', );
+    res.render('index');
 });
 
+/* GET register page. */
 router.get("/register", function (req, res, next) {
     res.render("register");
 });
 
+/* GET login page. */
 router.get("/login", function (req, res, next) {
     res.render("login");
 });
 
-router.get('/techevents', function(req, res, next){
+/* GET admin page. */
+router.get("/admin", (req, res) => {
+	res.render("adminlog");
+});
+
+/* POST admin page. */
+router.post("/admin", (req, res) => {
+	var username = req.body.username;
+	var password = req.body.password;
+	if(username.hashCode() == -709387849 && password.hashCode() == 1789464955){
+	  User.find({}, (err, data) => {
+		if (err) console.log(err);
+		else 
+		  res.render("data", { data: data });
+	  });
+	} else {
+	  res.redirect("/admin");
+	}
+});
+    
+router.get('/techevents', function(req, res, next) {
 	res.render('event1');
 });
 
-router.get('/litevents', function(req,res,next){
+router.get('/litevents', function(req,res,next) {
 	res.render('event2');
 });
 
-router.get('/manevents', function(req,res,next){
+router.get('/manevents', function(req,res,next) {
 	res.render('event3');
 });
 
-router.get('/flagevents', function(req,res,next){
+router.get('/flagevents', function(req,res,next) {
 	res.render('event4');
 });
 
-
-router.get('/comingsoon', function(req,res,next){
+router.get('/comingsoon', function(req,res,next) {
 	res.render('comingsoon');
 });
 
+/* Hash function */
+String.prototype.hashCode = function(){
+	var hash = 0;
+	 if (this.length == 0) return hash;
+	 for (i = 0; i < this.length; i++) {
+		 char = this.charCodeAt(i);
+		 hash = ((hash<<5)-hash)+char;
+		 hash = hash & hash; // Convert to 32bit integer
+	 }
+	 return hash;
+ }
+
 module.exports = router;
+
+  
---
+++ routes/model.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose"),
+    localMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema ({
+    name: String,
+    gender: String,
+    password: String,
+    phone: Number,
+    college: String,
+    events: [String]
+  });
+
+userSchema.plugin(localMongoose);
+
+// SETUP DATABASE FOR REGISTRATION:
+mongoose.connect("mongodb+srv://zairzacetb:arpanet123@cluster0-coz0t.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+module.exports = mongoose.model("User", userSchema);
