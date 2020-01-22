d2d712451ebbf06bf4c4e52f861433e72080cf91
Biswajit <patrabiswajit133@gmail.com>
2020-01-22 17:58:11 +0530

    upgraded redirection

--- routes/handler.js
+++ routes/handler.js
@@ -44,7 +44,7 @@ router.post("/register", function (req, res) {
                 from: 'Perception Cet 2020',
                 to: 'patrabiswajit133@gmail.com',
                 subject: 'Welcome to Perception 2020',
-                text: `Hi, ${req.user.name},Go through all the events and register in your dream-events`
+                text: `Hi, ${req.user.name},Go through all the events and register in your dream-events`  //TODO:email sending
               }, function(error, info){
                 if (error) {
                   console.log("mail error",error);
